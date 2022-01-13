f9b6151c01bf6fa69ae238e8b1c8716531b5bf4f
tibalt <tibalt.zhao@gmail.com>
2022-01-13 13:12:55 +0800

    fix assert in combase.dll q==0

--- OPCClientToolKit/OPCClient.cpp
+++ OPCClientToolKit/OPCClient.cpp
@@ -56,11 +56,13 @@ void COPCClient::init()
 	*/
 	CoInitializeSecurity(NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_NONE, RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL);
 
-	HRESULT result = CoGetMalloc(MEMCTX_TASK, &iMalloc);
-	if (FAILED(result))
-	{
+  if(iMalloc == 0){
+	  HRESULT result = CoGetMalloc(MEMCTX_TASK, &iMalloc);
+	  if (FAILED(result))
+	  {
 		throw OPCException("CoGetMalloc failed");
-	}
+	  }
+  }
 }
 
 void COPCClient::stop()
